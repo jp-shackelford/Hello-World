a04f90849e9cf4d6203f5c71649e8fa8df650fd8
Joshua Shackelford <Joshua Shackelford>
2017-04-17 23:16:08 -0700

    Hello-World in Java

--- Hello.java
+++ Hello.java
@@ -2,15 +2,5 @@ public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello World!");
       System.out.println();
-      int x = 0;
-      int broke = 0;
-      while(broke == 0) {
-         if (Math.abs(x) < 0) {
-            System.out.println("The value of x was" + x);
-            System.out.println("absolutely strange");
-            broke = 1;
-         }
-         x--;
-      }
    }
 }
